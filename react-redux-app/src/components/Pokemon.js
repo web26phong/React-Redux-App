0aacb91e7931aa2a25b2ebeda47f19e1759aa133
Phong Yang <phongyang88@hotmail.com>
2020-01-23 18:00:08 -0600

    added error state when failed to load pokemon list. added some colors via css.

--- react-redux-app/src/components/Pokemon.js
+++ react-redux-app/src/components/Pokemon.js
@@ -11,7 +11,7 @@ const Pokemon = (props) => {
 
             {/* INITIAL STATE */}
             {!props.pokemonlist && !props.isLoading && props.error === "" && (
-                <p>Click the button! Cmon!! GOTTA CATCH'EM ALL!!!!!!</p>
+                <p className="individualPokemonP">Click the button! Cmon!! GOTTA CATCH'EM ALL!!!!!!</p>
             )}
 
             {/* LOADING STATE */}
@@ -21,17 +21,24 @@ const Pokemon = (props) => {
                 color="#00BFFF"
                 height={100}
                 width={100}
-                timeout={10000} //3 secs
+                timeout={10000} //10 secs
               />
             )}
 
             {/* LOADED STATE */}
             {!props.isLoading && props.pokemonlist && (
-            <div>
+            <div className="pokemonListDiv">
                 {props.pokemonlist.map((pokemon, index) => (
-                    <p key={index}>#{index+1}. {pokemon.name}</p>
+                    <p className="individualPokemonP" key={index}>#{index+1}. {pokemon.name}</p>
                 ))}
             </div>
+            )}
+
+            {/* ERROR STATE */}
+            {props.error !== "" && (
+                <div>
+                    <p>{props.error}</p>
+                </div>
             )}
         </div>
     )
